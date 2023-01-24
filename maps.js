721991b1670e42bf89848bfa79df65c43d36ce75
ivo <ivozayas2014@gmail.com>
2023-01-24 09:10:54 -0300

    agregue un nivel y emprolije un par de cosas

--- maps.js
+++ maps.js
@@ -1,67 +1,66 @@
-
 const emojis = {
-    '-': ' ',
-    'O': '🚪',
-    'X': '💣',
-    'I': '🎁',
-    'PLAYER': '💀',
-    'BOMB_COLLISION': '🔥',
-    'GAME_OVER': '👎',
-    'WIN': '🏆',
-    'HEART': '❤'
-  };
+  '-': ' ',
+  'O': '🚪',
+  'X': '💣',
+  'I': '🎁',
+  'PLAYER': '💀',
+  'BOMB_COLLISION': '🔥',
+  'GAME_OVER': '👎',
+  'WIN': '🏆',
+  'HEART': '❤'
+}
   
-  const maps = [];
+const maps = []
   
-  maps.push(`
-    IXXXXXXXXX
-    -XXXXXXXXX
-    -XXXXXXXXX
-    -XXXXXXXXX
-    -XXXXXXXXX
-    -XXXXXXXXX
-    -XXXXXXXXX
-    -XXXXXXXXX
-    -XXXXXXXXX
-    OXXXXXXXXX`
-   );
-  maps.push(`
-    O--XXXXXXX
-    X--XXXXXXX
-    XX----XXXX
-    X--XX-XXXX
-    X-XXX--XXX
-    X-XXXX-XXX
-    XX--XX--XX
-    XX--XXX-XX
-    XXXX---IXX
-    XXXXXXXXXX
-    `);
-  maps.push(`
-    I-----XXXX
-    XXXXX-XXXX
-    XX----XXXX
-    XX-XXXXXXX
-    XX-----XXX
-    XXXXXX-XXX
-    XX-----XXX
-    XX-XXXXXXX
-    XX-----OXX
-    XXXXXXXXXX
-  `);
-  maps.push(`
-    O--XX-XXXX
-    XX-X--XXXX
-    XX-X----XX
-    X----XX--X
-    X-XXX--X-X
-    X--XXX---X
-    XX--XX-XXX
-    XXXXX--XXX
-    XXX----XXX
-    XXI-XXXXXX
-  `);
-  maps.push(`
+maps.push(`
+  IXXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  -XXXXXXXXX
+  OXXXXXXXXX
+`)
+maps.push(`
+  O--XXXXXXX
+  X--XXXXXXX
+  XX----XXXX
+  X--XX-XXXX
+  X-XXX--XXX
+  X-XXXX-XXX
+  XX--XX--XX
+  XX--XXX-XX
+  XXXX---IXX
+  XXXXXXXXXX
+`)
+maps.push(`
+  I-----XXXX
+  XXXXX-XXXX
+  XX----XXXX
+  XX-XXXXXXX
+  XX-----XXX
+  XXXXXX-XXX
+  XX-----XXX
+  XX-XXXXXXX
+  XX-----OXX
+  XXXXXXXXXX
+`)
+maps.push(`
+  O--XX-XXXX
+  XX-X--XXXX
+  XX-X----XX
+  X----XX--X
+  X-XXX--X-X
+  X--XXX---X
+  XX--XX-XXX
+  XXXXX--XXX
+  XXX----XXX
+  XXI-XXXXXX
+`)
+maps.push(`
   I-----XX-X
   XX-XX--XXX
   X-XX-X-X--
@@ -72,7 +71,19 @@ const emojis = {
   ---XXXXX-X
   -XXXXX---X
   --O----XXX
-`);
+`)
+maps.push(`
+  OXXXI-----
+  -XX-XXXXX-
+  -XXX--XXX-
+  -XXXX-----
+  -XXXXXXX-X
+  -XX------X
+  -XX-XXXXXX
+  -XX-------
+  -XXXXXXXX-
+  ----------
+`)
 
 const winMap = `
 XXXXXXXXXX
